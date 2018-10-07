b39ab0650173183c822f33dbc782e7a00a6e7bf1
calachaqui <san_calachaqui32@hotmail.com>
2018-10-06 20:26:35 -0600

    regular update, deleted a bunch of stuff

--- scheduler_logger.py
+++ scheduler_logger.py
@@ -19,19 +19,20 @@ owm = pyowm.OWM('a9bd34ad88582d8e147c150249581f0b')  # You MUST provide a valid 
 # Search for current weather in Orem (US)
 observation = owm.weather_at_place('Orem,us')
 w = observation.get_weather()
-print(w)                      # <Weather - reference time=2013-12-18 09:20,
+#print(w)                      # <Weather - reference time=2013-12-18 09:20,
                               # status=Clouds>
 
 # Weather details
-print w.get_wind()                  # {'speed': 4.6, 'deg': 330}
-print w.get_humidity()              # 87
+#print w.get_wind()                  # {'speed': 4.6, 'deg': 330}
+#print w.get_humidity()              # 87
 orem_temp = w.get_temperature('fahrenheit')  # {'temp_max': 10.5, 'temp': 9.7, 'temp_min': 9.0}
-print orem_temp['temp']
+#print orem_temp['temp']
 oct = orem_temp['temp']
 
+
 ## uncomment this section to write out to log
-#with open('/home/pi/python_scripts/logged_dates.txt','a') as the_log:
-#       the_log.write(str(right_now)+"	"+str(oct)+"\n")
+with open('/home/pi/compute/run_log.txt','a') as the_log:
+       the_log.write(str(right_now)+"	"+str(oct)+"\n")
 
 
 #orem utah observations
